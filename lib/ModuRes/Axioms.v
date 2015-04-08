629ef6dafde007d96f9c845674078238178180b8
Ralf Jung <jung@mpi-sws.org>
2015-04-08 13:56:46 +0200

    make sure we don't have a typo in our axiom of proof irrelevance, by using the one from the standard library

--- lib/ModuRes/Axioms.v
+++ lib/ModuRes/Axioms.v
@@ -1,4 +1,5 @@
 (* This file defines all axioms we are relying on in our development. *)
+Require Import Coq.Logic.ProofIrrelevance.
 
-Axiom ProofIrrelevance: forall (P: Prop) (p q: P), p = q.
+Definition ProofIrrelevance := proof_irrelevance.
 Ltac pi p q := erewrite (ProofIrrelevance _ p q).
