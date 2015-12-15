100a7b9dcaab961f9481ab63764e0bd24c3ed6ae
Robbert Krebbers <mail@robbertkrebbers.nl>
2015-12-15 13:34:28 +0100

    Extensionality of map on setoid equality on finite maps.

--- prelude/fin_maps.v
+++ prelude/fin_maps.v
@@ -536,6 +536,12 @@ Proof. apply map_eq; intros i; by rewrite lookup_fmap, option_fmap_id. Qed.
 Lemma map_fmap_compose {A B C} (f : A → B) (g : B → C) (m : M A) :
   g ∘ f <$> m = g <$> f <$> m.
 Proof. apply map_eq; intros i; by rewrite !lookup_fmap,option_fmap_compose. Qed.
+Lemma map_fmap_setoid_ext `{Equiv A, Equiv B} (f1 f2 : A → B) m :
+  (∀ i x, m !! i = Some x → f1 x ≡ f2 x) → f1 <$> m ≡ f2 <$> m.
+Proof.
+  intros Hi i; rewrite !lookup_fmap.
+  destruct (m !! i) eqn:?; constructor; eauto.
+Qed.
 Lemma map_fmap_ext {A B} (f1 f2 : A → B) m :
   (∀ i x, m !! i = Some x → f1 x = f2 x) → f1 <$> m = f2 <$> m.
 Proof.
