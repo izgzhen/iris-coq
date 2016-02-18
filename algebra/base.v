ab2b5816cea1c309d33e31f89bfa112c9b1966cc
Ralf Jung <jung@mpi-sws.org>
2016-02-18 14:09:24 +0100

    turns out we can import with shorter names (and we actually already do that frequently in "From Coq Import")

--- algebra/base.v
+++ algebra/base.v
@@ -1,4 +1,4 @@
-From mathcomp.ssreflect Require Export ssreflect.
+From mathcomp Require Export ssreflect.
 From prelude Require Export prelude.
 Global Set Bullet Behavior "Strict Subproofs".
 Global Open Scope general_if_scope.
