2ba9728f98573d4a63823e8d7b7422eb6d1d984e
Robbert Krebbers <mail@robbertkrebbers.nl>
2016-02-27 02:12:58 +0100

    Improve notation for Case.

--- heap_lang/notation.v
+++ heap_lang/notation.v
@@ -25,7 +25,8 @@ Coercion of_val : val >-> expr.
    pretty printing. *)
 Notation "( e1 , e2 , .. , en )" := (Pair .. (Pair e1 e2) .. en) : lang_scope.
 Notation "' l" := (Lit l%Z) (at level 8, format "' l").
-Notation "'match:' e0 'with' x1 => e1 | x2 => e2 'end'" := (Case e0 x1 e1 x2 e2)
+Notation "'match:' e0 'with' 'injL' x1 => e1 | 'injR' x2 => e2 'end'" :=
+  (Case e0 x1 e1 x2 e2)
   (e0, x1, e1, x2, e2 at level 200) : lang_scope.
 Notation "' l" := (LitV l%Z) (at level 8, format "' l").
 Notation "'()"  := (Lit LitUnit) (at level 0).
