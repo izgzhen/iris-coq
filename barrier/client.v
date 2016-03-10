3f520e3babf1f13a902e9357f501dd74443c5619
Ralf Jung <jung@mpi-sws.org>
2016-03-10 17:35:06 +0100

    go back to # for values, now that this does not look like wp anymore

--- barrier/client.v
+++ barrier/client.v
@@ -4,11 +4,11 @@ From iris.program_logic Require Import auth sts saved_prop hoare ownership.
 Import uPred.
 
 Definition worker (n : Z) : val :=
-  λ: "b" "y", ^wait '"b" ;; !'"y" §n.
+  λ: "b" "y", ^wait '"b" ;; !'"y" #n.
 Definition client : expr [] :=
-  let: "y" := ref §0 in
-  let: "b" := ^newbarrier §() in
-  ('"y" <- (λ: "z", '"z" + §42) ;; ^signal '"b") ||
+  let: "y" := ref #0 in
+  let: "b" := ^newbarrier #() in
+  ('"y" <- (λ: "z", '"z" + #42) ;; ^signal '"b") ||
     (^(worker 12) '"b" '"y" || ^(worker 17) '"b" '"y").
 
 Section client.
@@ -16,7 +16,7 @@ Section client.
   Local Notation iProp := (iPropG heap_lang Σ).
 
   Definition y_inv q y : iProp :=
-    (∃ f : val, y ↦{q} f ★ □ ∀ n : Z, WP f §n {{ λ v, v = §(n + 42) }})%I.
+    (∃ f : val, y ↦{q} f ★ □ ∀ n : Z, WP f #n {{ λ v, v = #(n + 42) }})%I.
 
   Lemma y_inv_split q y :
     y_inv q y ⊢ (y_inv (q/2) y ★ y_inv (q/2) y).
@@ -60,7 +60,7 @@ Section client.
       (ewp eapply wp_store); eauto with I. strip_later.
       ecancel [y ↦ _]%I. apply wand_intro_l.
       wp_seq. rewrite -signal_spec right_id assoc sep_elim_l comm.
-      apply sep_mono_r. rewrite /y_inv -(exist_intro (λ: "z", '"z" + §42)%V).
+      apply sep_mono_r. rewrite /y_inv -(exist_intro (λ: "z", '"z" + #42)%V).
       apply sep_intro_True_r; first done. apply: always_intro.
       apply forall_intro=>n. wp_let. wp_op. by apply const_intro.
     - (* The two spawned threads, the waiters. *)
