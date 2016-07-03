f70dd438615606c3cfe216b72b69409084524e8e
Robbert Krebbers <mail@robbertkrebbers.nl>
2016-07-03 16:02:08 +0200

    Mononicity of auth_own.

--- program_logic/auth.v
+++ program_logic/auth.v
@@ -55,6 +55,10 @@ Section auth.
 
   Lemma auth_own_op γ a b : auth_own γ (a ⋅ b) ⊣⊢ auth_own γ a ★ auth_own γ b.
   Proof. by rewrite /auth_own -own_op auth_frag_op. Qed.
+
+  Lemma auth_own_mono γ a b : a ≼ b → auth_own γ b ⊢ auth_own γ a.
+  Proof. intros [? ->]. by rewrite auth_own_op sep_elim_l. Qed.
+
   Lemma auth_own_valid γ a : auth_own γ a ⊢ ✓ a.
   Proof. by rewrite /auth_own own_valid auth_validI. Qed.
 
