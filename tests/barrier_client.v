fd86b7bb789277fe7cb131689069e83216c0aad4
Ralf Jung <jung@mpi-sws.org>
2016-08-19 13:54:35 +0200

    do "Print Assumptions" outside of sections

--- tests/barrier_client.v
+++ tests/barrier_client.v
@@ -59,7 +59,7 @@ End client.
 
 Section ClosedProofs.
 
-Definition Σ : gFunctors := #[ heapΣ ; barrierΣ ; spawnΣ ].
+Let Σ : gFunctors := #[ heapΣ ; barrierΣ ; spawnΣ ].
 
 Lemma client_adequate σ : adequate client σ (λ _, True).
 Proof.
@@ -67,6 +67,6 @@ Proof.
   apply (client_safe (nroot .@ "barrier")); auto with ndisj.
 Qed.
 
-Print Assumptions client_adequate.
-
 End ClosedProofs.
+
+Print Assumptions client_adequate.
