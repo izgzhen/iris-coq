d04287bc50ba4b4f1536eb33b930cb2dccaa1280
Ralf Jung <jung@mpi-sws.org>
2016-11-22 17:19:30 +0100

    make deprecated notation only parsing

--- algebra/deprecated.v
+++ algebra/deprecated.v
@@ -3,4 +3,4 @@ From iris.algebra Require Import ofe.
 (* Old notation for backwards compatibility. *)
 
 (* Deprecated 2016-11-22. Use ofeT instead. *)
-Notation cofeT := ofeT.
+Notation cofeT := ofeT (only parsing).
