cd9d4a5bcca654d7a629565f11bf562403e71b2f
Ralf Jung <jung@mpi-sws.org>
2016-12-22 10:46:33 +0100

    fix build

--- theories/heap_lang/adequacy.v
+++ theories/heap_lang/adequacy.v
@@ -1,4 +1,4 @@
-From iris.program_logic Require Export weakestpre adequacy gen_heap.
+From iris.program_logic Require Export weakestpre adequacy.
 From iris.heap_lang Require Export lifting.
 From iris.algebra Require Import auth.
 From iris.heap_lang Require Import proofmode notation.
