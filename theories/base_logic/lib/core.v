48371b504295c8620d1d8e973b9b9ff750c5d6b6
Ralf Jung <jung@mpi-sws.org>
2016-12-22 10:46:39 +0100

    Implement JH's idea of a "core of an assertion"

---
+++ theories/base_logic/lib/core.v
@@ -0,0 +1,39 @@
+From iris.base_logic Require Import base_logic.
+From iris.proofmode Require Import tactics.
+Import uPred.
+
+(** The "core" of an assertion is its maximal persistent part.
+    It can be defined entirely within the logic... at least
+    in the shallow embedding. *)
+
+Definition coreP {M : ucmraT} (P : uPred M) : uPred M :=
+  (∀ (Q : uPred M) `(!PersistentP Q, !P -∗ Q), Q)%I.
+
+Section core.
+  Context {M : ucmraT}.
+  Implicit Types P Q : uPred M.
+
+  Lemma coreP_intro P :
+    P -∗ coreP P.
+  Proof.
+    iIntros "HP". iIntros (Q HQ HPQ). by iApply HPQ.
+  Qed.
+
+  Global Instance coreP_persistent P :
+    PersistentP (coreP P).
+  Proof.
+    iIntros "HCP". iApply always_forall. iIntros (Q).
+    iApply always_forall. iIntros (HQ). iApply always_forall.
+    iIntros (HPQ). iApply HQ. unshelve iApply ("HCP" $! Q). done.
+  Qed.
+
+  Lemma corP_elim P :
+    PersistentP P → coreP P -∗ P.
+  Proof.
+    iIntros (?) "HCP". unshelve iApply ("HCP" $! P).
+    iIntros "P". done.
+  Qed.
+End core.
+
+
+
