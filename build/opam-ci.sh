48f8e00868b6c1b01c450a7ad9ce90fd62d29dae
Ralf Jung <jung@mpi-sws.org>
2017-02-20 15:23:00 +0100

    use new CI machine

--- build/opam-ci.sh
+++ build/opam-ci.sh
@@ -4,16 +4,19 @@ set -e
 
 # Prepare OPAM configuration
 export OPAMROOT="$(pwd)/opamroot"
-export OPAMJOBS=16
+export OPAMJOBS="$((2*$CPU_CORES))"
 export OPAM_EDITOR="$(which false)"
 
 # Make sure we got a good OPAM
 test -d "$OPAMROOT" || (mkdir "$OPAMROOT" && opam init --no-setup -y)
 eval `opam conf env`
+if test $(find "$OPAMROOT/repo/package-index" -mtime 1); then
+    # last update was more than a day ago
+    opam update
+fi
 test -d "$OPAMROOT/repo/coq-extra-dev" || opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev -p 5
 test -d "$OPAMROOT/repo/coq-core-dev" || opam repo add coq-core-dev https://coq.inria.fr/opam/core-dev -p 5
 test -d "$OPAMROOT/repo/coq-released" || opam repo add coq-released https://coq.inria.fr/opam/released -p 10
-opam update
 
 # Install fixed versions of some dependencies
 echo
