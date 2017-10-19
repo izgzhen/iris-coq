53d0af97a72cd363c52881141bacd111d4819f16
Ralf Jung <jung@mpi-sws.org>
2017-10-19 17:28:26 +0200

    move barrier to new iris-examples repo; test that repo after each Iris commit

--- theories/heap_lang/lib/barrier/barrier.v
+++
@@ -1,7 +0,0 @@
-From iris.heap_lang Require Export notation.
-Set Default Proof Using "Type".
-
-Definition newbarrier : val := λ: <>, ref #false.
-Definition signal : val := λ: "x", "x" <- #true.
-Definition wait : val :=
-  rec: "wait" "x" := if: !"x" then #() else "wait" "x".
