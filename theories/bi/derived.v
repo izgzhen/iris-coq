amintimany/iris

Simplify box axioms.
Thanks to discussions with Ales and Amin.

theories/bi/derived.v
@@ -870,6 +870,15 @@ Proof. intros P Q; apply always_mono. Qed.
 Global Instance always_absorbing P : Absorbing (□ P).
 Proof. rewrite /Absorbing=> R. apply always_absorbing. Qed.
 
+Lemma always_and_sep_assoc_1 P Q R : □ P ∧ (Q ∗ R) ⊢ (□ P ∧ Q) ∗ R.
+Proof.
+  rewrite {1}always_idemp_2 always_and_sep_elim assoc.
+  apply sep_mono_l, and_intro.
+  - by rewrite and_elim_r absorbing.
+  - by rewrite and_elim_l left_id.
+Qed.
+Lemma always_and_emp_elim P : emp ∧ □ P ⊢ P.
+Proof. by rewrite comm always_and_sep_elim right_id and_elim_r. Qed.
 Lemma always_elim P : □ P ⊢ P ∗ True.
 Proof.
   rewrite -(right_id True%I _ (□ _)%I) -{1}(left_id emp%I _ True%I).

theories/bi/interface.v
@@ -107,10 +107,8 @@ Section bi_mixin.
     bi_mixin_always_exist_1 {A} (Ψ : A → PROP) : □ (∃ a, Ψ a) ⊢ ∃ a, □ Ψ a;
 
     bi_mixin_always_emp_intro P : P ⊢ □ emp;
-    bi_mixin_always_and_emp_elim P : emp ∧ □ P ⊢ P;
-
     bi_mixin_always_absorbing P Q : □ P ∗ Q ⊢ □ P;
-    bi_mixin_always_and_sep_assoc_1 P Q R : □ P ∧ (Q ∗ R) ⊢ (□ P ∧ Q) ∗ R;
+    bi_mixin_always_and_sep_elim P Q : □ P ∧ Q ⊢ (emp ∧ P) ∗ Q;
   }.
 
   Record SBIMixin := {
@@ -412,12 +410,10 @@ Proof. eapply bi_mixin_always_exist_1, bi_bi_mixin. Qed.
 
 Lemma always_emp_intro P : P ⊢ □ emp.
 Proof. eapply bi_mixin_always_emp_intro, bi_bi_mixin. Qed.
-Lemma always_and_emp_elim P : emp ∧ □ P ⊢ P.
-Proof. eapply bi_mixin_always_and_emp_elim, bi_bi_mixin. Qed.
 Lemma always_absorbing P Q : □ P ∗ Q ⊢ □ P.
 Proof. eapply (bi_mixin_always_absorbing bi_entails), bi_bi_mixin. Qed.
-Lemma always_and_sep_assoc_1 P Q R : □ P ∧ (Q ∗ R) ⊢ (□ P ∧ Q) ∗ R.
-Proof. eapply bi_mixin_always_and_sep_assoc_1, bi_bi_mixin. Qed.
+Lemma always_and_sep_elim P Q : □ P ∧ Q ⊢ (emp ∧ P) ∗ Q.
+Proof. eapply bi_mixin_always_and_sep_elim, bi_bi_mixin. Qed.
 End bi_laws.
 
 Section sbi_laws.

izgzhen/iris-coq

theories/bi/derived.v
@@ -872,6 +872,15 @@ Proof. intros P Q; apply persistently_mono. Qed.
 Global Instance persistently_absorbing P : Absorbing (□ P).
 Proof. rewrite /Absorbing=> R. apply persistently_absorbing. Qed.
 
+Lemma persistently_and_sep_assoc_1 P Q R : □ P ∧ (Q ∗ R) ⊢ (□ P ∧ Q) ∗ R.
+Proof.
+  rewrite {1}persistently_idemp_2 persistently_and_sep_elim assoc.
+  apply sep_mono_l, and_intro.
+  - by rewrite and_elim_r absorbing.
+  - by rewrite and_elim_l left_id.
+Qed.
+Lemma persistently_and_emp_elim P : emp ∧ □ P ⊢ P.
+Proof. by rewrite comm persistently_and_sep_elim right_id and_elim_r. Qed.
 Lemma persistently_elim P : □ P ⊢ P ∗ True.
 Proof.
   rewrite -(right_id True%I _ (□ _)%I) -{1}(left_id emp%I _ True%I).

theories/bi/interface.v
@@ -111,10 +111,8 @@ Section bi_mixin.
       □ (∃ a, Ψ a) ⊢ ∃ a, □ Ψ a;
 
     bi_mixin_persistently_emp_intro P : P ⊢ □ emp;
-    bi_mixin_persistently_and_emp_elim P : emp ∧ □ P ⊢ P;
-
     bi_mixin_persistently_absorbing P Q : □ P ∗ Q ⊢ □ P;
-    bi_mixin_persistently_and_sep_assoc_1 P Q R : □ P ∧ (Q ∗ R) ⊢ (□ P ∧ Q) ∗ R;
+    bi_mixin_persistently_and_sep_elim P Q : □ P ∧ Q ⊢ (emp ∧ P) ∗ Q;
   }.
 
   Record SBIMixin := {
@@ -420,12 +418,10 @@ Proof. eapply bi_mixin_persistently_exist_1, bi_bi_mixin. Qed.
 
 Lemma persistently_emp_intro P : P ⊢ □ emp.
 Proof. eapply bi_mixin_persistently_emp_intro, bi_bi_mixin. Qed.
-Lemma persistently_and_emp_elim P : emp ∧ □ P ⊢ P.
-Proof. eapply bi_mixin_persistently_and_emp_elim, bi_bi_mixin. Qed.
 Lemma persistently_absorbing P Q : □ P ∗ Q ⊢ □ P.
 Proof. eapply (bi_mixin_persistently_absorbing bi_entails), bi_bi_mixin. Qed.
-Lemma persistently_and_sep_assoc_1 P Q R : □ P ∧ (Q ∗ R) ⊢ (□ P ∧ Q) ∗ R.
-Proof. eapply bi_mixin_persistently_and_sep_assoc_1, bi_bi_mixin. Qed.
+Lemma persistently_and_sep_elim P Q : □ P ∧ Q ⊢ (emp ∧ P) ∗ Q.
+Proof. eapply bi_mixin_persistently_and_sep_elim, bi_bi_mixin. Qed.
 End bi_laws.
 
 Section sbi_laws.