09741a0349b9746daa429652fc8e91d674caee2b
Robbert Krebbers <mail@robbertkrebbers.nl>
2017-12-20 12:09:33 +0100

    Comment on the `uPred` module trick.

--- theories/base_logic/base_logic.v
+++ theories/base_logic/base_logic.v
@@ -1,6 +1,9 @@
 From iris.base_logic Require Export derived.
 Set Default Proof Using "Type".
 
+(* The trick of having multiple [uPred] modules, which are all exported in
+another [uPred] module is by Jason Gross and described in:
+https://sympa.inria.fr/sympa/arc/coq-club/2016-12/msg00069.html *)
 Module Import uPred.
   Export upred.uPred.
   Export primitive.uPred.
