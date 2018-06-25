42f0842f3521add9d057818979a53f99107dcbff
Ralf Jung <jung@mpi-sws.org>
2018-06-25 14:02:15 +0200

    bump std++; test that we don't break setoid_rewrite

--- tests/algebra.v
+++ tests/algebra.v
@@ -8,3 +8,19 @@ Section tests.
   Solve Obligations with solve_proper.
 
 End tests.
+
+(** Check that [@Reflexive Prop ?r] picks the instance setoid_rewrite needs.
+    Really, we want to set [Hint Mode Reflexive] in a way that this fails, but
+    we cannot [1].  So at least we try to make sure the first solution found
+    is the right one, to not pay performance in the success case [2].
+
+    [1] https://github.com/coq/coq/issues/7916
+    [2] https://gitlab.mpi-sws.org/robbertkrebbers/coq-stdpp/merge_requests/38
+*)
+Lemma test_setoid_rewrite :
+  exists R, @Reflexive Prop R /\ R = iff.
+Proof.
+  eexists. split.
+  - apply _.
+  - reflexivity.
+Qed.
